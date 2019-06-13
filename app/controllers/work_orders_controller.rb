651e7e4ab3d3ecd1ef6f9e9509cb47ef1e0bbf5e
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-06-13 22:56:51 +0300

    add controllers

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -1,4 +1,21 @@
 class WorkOrdersController < ApplicationController
   def new
+    @work_order = WorkOrder.new
+  end
+
+  def create
+    @work_order = work_orders.build(work_orders_params)
+    if @work_order.save
+      flash[:success] = 'Work Order successfully added'
+    else
+      flash[:alert] = 'Work Order couldn\'t be created'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def work_orders_params
+    params.require(:work_order).permit(:title, :description, :deadline)
   end
 end
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,4 +1,42 @@
 class WorkersController < ApplicationController
   def new
+    @worker = Worker.new
+  end
+
+  def create
+    @worker = worker.build(worker_params)
+    if @worker.save
+      flash[:success] = 'worker successfully created'
+    else
+      flash[:alert] = 'worker couldn\'t be created'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @worker = Worker.find_by(id: params[:id])
+    @worker.destroy
+    flash[:success] = 'Worker deleted'
+    redirect_back(fallback_location: root_path)
+  end
+
+  def update
+    @worker = Worker.find(params[:id])
+    if @worker.update_attributes(work_params)
+      redirect_to @worker
+      flash[:success] = "Worker assigned to work order"
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def worker_params
+    params.require(:worker).permit(:name, :company_name, :email)
+  end
+
+  def work_params
+    params.require(:worker).permit(:work_order_id)
   end
 end
