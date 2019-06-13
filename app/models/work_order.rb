40392f117ee32c2406e333bc4be8155f300bc69b
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-06-13 16:59:08 +0300

    add validations and associations

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -1,2 +1,14 @@
 class WorkOrder < ApplicationRecord
+  NUMBER_OF_PERMITTED_WORKERS = 5
+  has_many :workers, dependent: :destroy, before_add: :validate_worker_limit
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 600 }
+  validates :deadline, presence: true
+
+  private
+
+  def validate_worker_limit(worker)
+    raise Exception.new if workers.size >= NUMBER_OF_PERMITTED_WORKERS
+  end
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,10 @@
 class Worker < ApplicationRecord
+  belongs_to :work_order
+
+  validates :name, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
